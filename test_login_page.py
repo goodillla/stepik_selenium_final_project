8a00f095638f70118335cbb07b99d470573f62b7
Igor Skobeev <goodillla@gmail.com>
2023-02-06 17:09:56 +0100

    login page tests added

---
+++ test_login_page.py
@@ -0,0 +1,28 @@
+from .pages.login_page import LoginPage
+
+'''
+# мне не нравится что тут запускается сразу 3 теста 
+def test_should_be_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
+'''
+
+def test_login_url_is_correct(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+
+def test_login_form_exist(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+def test_register_form_exist(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
