5bbd6166f69a5721e21dd2cbaa9cb6889c024fff
Igor Skobeev <goodillla@gmail.com>
2023-02-06 19:57:54 +0100

    + xfail + headless mode

--- conftest.py
+++ conftest.py
@@ -7,17 +7,22 @@ def pytest_addoption(parser):
                      help="Choose browser: chrome or firefox")
     parser.addoption('--language', action='store', default="en",
                      help="Choose language: es")
+    parser.addoption('--headless', action='store', default='false',
+                     help="Open a browser invisible, without GUI is used by default")
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="module")  #scope"module" или function
 def browser(request):
     browser_name = request.config.getoption("browser_name")
     user_language = request.config.getoption("language")
+    headless = request.config.getoption('headless')
     browser = None
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
         options = Options()
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
         options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        if headless == 'true':
+            options.add_argument('headless')
         browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
--- test_product_page.py
+++ test_product_page.py
@@ -1,14 +1,15 @@
 from .pages.product_page import ProductPage
 import pytest
 
-n = 10  #зададим количество тестов
-links = ['http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer'+str(i) for i in range(n)]
-@pytest.mark.parametrize('link', links)
-@pytest.mark.parametrize('link', ["okay_link",
-                                  pytest.param("bugged_link", marks=pytest.mark.xfail),
-                                  "okay_link"])
-def test_guest_can_add_product_to_basket(browser, link):
-    #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/" #?promo=newYear убрал чтоб не решать каждый раз
+
+#n = 10  #зададим количество тестов
+#links = [i for i in range(n)]
+@pytest.mark.parametrize('n', [1, 2, 3, 4, 5, 6,\
+                                 pytest.param(7, marks=pytest.mark.xfail(reason="proger is too lazy fix it :)")),\
+                                 8, 9])
+def test_guest_can_add_product_to_basket(browser, n):
+    browser.delete_all_cookies()  #если захотим все сделать в одном браузере
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer'+str(n)
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_product()
